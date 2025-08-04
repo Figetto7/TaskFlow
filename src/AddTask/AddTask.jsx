278f915886ffbb4b15dcb4aefb5044ded2e5ea7f
Figetto7 <dargeniogabriele7@gmail.com>
2025-08-04 15:52:12 +0200

    PART 1 ADD TASK

--- src/AddTask/AddTask.jsx
+++ src/AddTask/AddTask.jsx
@@ -1,8 +1,18 @@
 import React from 'react'
 import styles from './AddTask.module.css'
+import { useNavigate } from 'react-router-dom'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faArrowLeft } from '@fortawesome/free-solid-svg-icons';
 
-export default function addTask (){
+
+export default function AddTask (){
+  const navigate = useNavigate();
   return (
-    <div className={styles.addTask}>ADD TASK WORKING</div>
+    <div className={styles.addTaskHeader}>
+      <button className={styles.backButtonHome} onClick={() => navigate('/')}> <FontAwesomeIcon icon={faArrowLeft} /> Torna alla Home</button>
+      <h1 className={styles.AddTasktitle}>Nuovo Task</h1>
+      <p className={styles.AddTasksubtitle}>Crea un nuovo task per organizzare meglio il tuo lavoro</p>
+    </div>
+    
   )
 }
