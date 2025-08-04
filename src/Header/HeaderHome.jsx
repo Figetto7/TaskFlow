dd1c69712cb65210d64eed1f6d1aefac08c9aebc
Figetto7 <dargeniogabriele7@gmail.com>
2025-08-04 09:54:54 +0200

    Adjusted styles

--- src/Header/HeaderHome.jsx
+++ src/Header/HeaderHome.jsx
@@ -1,13 +1,17 @@
 import styles from "./HeaderHome.module.css";
 
+import { useNavigate } from "react-router-dom";
+
 export default function HeaderHome () {
+  const navigate = useNavigate();
+
   return (
     <div className={styles.headerDiv}>
           <div>
             <h1 className={styles.title}>I tuoi Task</h1>
           <p className={styles.subTitle}>Gestisci e monitora i tuoi task quotidiani</p>
           </div>
-          <button className={styles.addTask}> + Nuovo Task</button>
+          <button className={styles.addTask} onClick={() => navigate("/AddTask")}> + Nuovo Task</button>
         </div>
   )
 }
